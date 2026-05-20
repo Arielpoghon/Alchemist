c979d5eb4a63f1b4b7c65c7e83a3c89e006aa487
Ariel Poghon <mnangatariel27@gmail.com>
2026-05-20 03:56:25 +0300

    Update api-gateway.js

--- src/api-gateway.js
+++ src/api-gateway.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 require('dotenv').config();
 
 const app = express();
-app.use(express.json({ limit: '1mb' }));
+app.use(express.json({ limit: '1mb' })); 
 
 const API_PORT = Number(process.env.API_PORT || 8000);
 const API_HOST = process.env.API_HOST || '0.0.0.0';
